71d406940238311ae8dd262d747dbf01a5479f82
GeorgeRaduta <k1332897@kcl.ac.uk>
2016-06-13 01:30:11 +0100

    Javac plus user message

--- runIO.js
+++ runIO.js
@@ -17,6 +17,105 @@ app.listen(3001);
 
 console.log("Server running on http://localhost:3001/");
 
+//STUDENT LOGOUT POST REQUEST
+app.post('/check-educator-code', function(req, res) {
+	console.log(req.body);
+	var input = req.body.inputArary;
+	var output = req.body.outputArray;
+	var code = req.body.code;
+	var className = "HelloWorld";
+	//Evaluate source
+	var results = getResultsCode(input, output, code, className);
+
+	res.json(results);
+	// res.json("Got it, cocos");
+});
+
+function getResultsCode(input, output, code, className) {
+	var objToReturn = {
+		compiled: {
+			bool: true,
+			error: ""
+		},
+		resultsArray: []
+	}
+	var nameOfFileExtension = className + ".java";
+	fs.writeFileSync('TestingEnvironment/' + nameOfFileExtension, code);
+	//TODO : Add verification for correctly creating the file;
+
+	//JAVAC 
+	var javacExecute = spawnSync('javac', [nameOfFileExtension], {cwd:'TestingEnvironment', timeout:2000});
+	if (!(javacExecute.status == 0)) {
+		//get errors to the user
+		if (!(javacExecute.stderr.toString() == "")) {
+			objToReturn.compiled.bool = false;
+			objToReturn.compiled.error = javacExecute.stderr.toString(); 
+			return objToReturn;
+			// printErrors(javacExecute.stderr.toString(), 'compiling');
+		} else if (!(javacExecute.error == null)) {
+			objToReturn.compiled.bool = false;
+			objToReturn.compiled.error = javaExecute.error; 
+			return objToReturn;
+			// printErrors(javaExecute.error, 'compiling');
+		} 
+		else {
+			objToReturn.compiled.bool = false;
+			objToReturn.compiled.error = 'Unkwown Error';
+			return objToReturn;
+			// console.log('Unkown Error');
+		}
+	}
+	// } else {
+	// 	//To execute for every single test provided by the educator
+	// 	for (var i = 0; i < arrayOfInput.length; ++i) {
+	// 		var inputTest = arrayOfInput[i];
+
+	// 		var javaExecute = spawnSync('java', [nameOfFile], {
+	// 		// input: inputTest,
+	// 		cwd:'TestingEnvironment', 
+	// 		timeout:2000
+	// 		}); 
+
+	// 		if (!(javaExecute.status == 0)) {
+	// 		//get errors to the user
+	// 			if (!(javaExecute.stderr.toString() == "")) {
+	// 				printErrors(javaExecute.stderr.toString(), 'running');
+	// 			} else if (!(javaExecute.error == null)) {
+	// 				printErrors(javaExecute.error, 'running');
+	// 			} 
+	// 			else {
+	// 				console.log('Unkown Error');
+	// 			}
+	// 		} else {
+	// 			//No Errors Branch
+	// 			//Compare user's input with educator's input and provide feedback
+	// 			// var obj = {
+	// 			// 	index : i,
+	// 			// 	message: "Message",
+	// 			// 	result: output,
+	// 			// 	wrong: true/false
+	// 			// }
+	// 			console.log(javaExecute.stdout.toString());
+	// 		}
+	// 	}
+	// }
+
+	// //Delete files from testing environment
+	// try {
+	// 	fs.unlinkSync('TestingEnvironment/' + nameOfFile + '.class');
+	// } catch(err) {
+	// 	console.log(err);
+	// }	
+	// try {
+	// 	fs.unlinkSync('TestingEnvironment/' + nameOfFile + '.java');
+	// } catch(err) {
+	// 	console.log(err);
+	// }
+	//Return error if compilation/runs fails
+	return objToReturn;
+}
+//Functions to deal with evaluation
+//TODO Queue implementation
 function readPath() {
 	fs.readFileSync('sourcesPath', 'utf8', function (err,data) {
 		if (err) {
