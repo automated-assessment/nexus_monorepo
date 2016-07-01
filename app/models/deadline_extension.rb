5c35f4b1dd140bbc4b94756b2f90e4e4aba4b5b6
Sam White <sam.white@kcl.ac.uk>
2016-07-01 10:44:28 +0100

    refine error message

--- app/models/deadline_extension.rb
+++ app/models/deadline_extension.rb
@@ -4,8 +4,13 @@ class DeadlineExtension < ActiveRecord::Base
 
   validates :extendeddeadline, presence: true
   validate :extension_must_be_temporally_after_deadline
+  validate :not_duplicate?
 
   def extension_must_be_temporally_after_deadline
     errors.add(:extendeddeadline, 'Must be after the regular deadline') if extendeddeadline < assignment.deadline
+  end
+
+  def not_duplicate?
+    errors.add(:user, 'already has an extension for this assignment') unless DeadlineExtension.where(user: user, assignment: assignment).empty?
   end
 end
