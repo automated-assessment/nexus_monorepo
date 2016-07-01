b16a1e3bcc49821a7a1ea3f4c6ef13cc5096bae3
Sam White <sam.white@kcl.ac.uk>
2016-07-02 00:29:29 +0100

    Pre-production changes (PGSQL, dev/prod separate dockerfiles)

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,3 +1,3 @@
 # Required so that timestamps retrieved from the database can be sorted precisely
 # e.g. to ensure audit items on submission are displayed in the correct order
-Time::DATE_FORMATS[:db] = '%Y-%m-%d %H:%M:%S.%3N'
+Time::DATE_FORMATS[:db] = '%Y-%m-%d %H:%M:%S.%3N' unless Rails.env.production?
