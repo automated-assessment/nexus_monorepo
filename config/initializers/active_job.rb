2b25f8296ea4095b827dc81f9663eb9ed368183b
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2016-07-13 15:19:31 +0100

    Ensure queues are durable and persistent

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -4,9 +4,10 @@ ActiveJob::Base.queue_adapter = :sneakers
 
 opts = {
   amqp: "amqp://#{Rails.configuration.rabbit_mq_host}:#{Rails.configuration.rabbit_mq_port}",
-  #vhost: 'guest',
   exchange: 'sneakers',
-  exchange_type: :direct
+  exchange_type: :direct,
+  durable: true,
+  ack: true
 }
 
 Sneakers.configure(opts)
