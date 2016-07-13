538c0970bc130e3a587d903fa4d3c0e325608b80
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2016-07-13 21:54:07 +0100

    Added management panel for failed submissions to admin panel.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @access_tokens = AccessToken.all
     @marking_tools = MarkingTool.all
     @user_count = User.all.count
+    @failed_submissions_count = Submission.where(failed: true).count
   end
 
   def user_list
