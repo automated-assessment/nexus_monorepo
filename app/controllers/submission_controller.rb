f1db2a8e7f7c7ce3e57c2a1da7baf2eed3060771
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2016-09-26 10:42:08 +0100

    Use standard logger rather than direct STDERR access

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -64,7 +64,7 @@ class SubmissionController < ApplicationController
 
     rescue StandardError => e
       # At this point, we need to remove the submission if anything goes wrong because we won't be able to recover yet
-      STDERR.puts "Error copying uploaded zip file: #{e.inspect}."
+      logger.error "Error copying uploaded zip file: #{e.inspect}."
 
       @submission.destroy
       redirect_to '/500'
@@ -126,7 +126,7 @@ class SubmissionController < ApplicationController
 
     rescue StandardError => e
       # At this point, we need to remove the submission if anything goes wrong because we won't be able to recover yet
-      STDERR.puts "Error copying files from Web IDE: #{e.inspect}."
+      logger.error "Error copying files from Web IDE: #{e.inspect}."
 
       @submission.destroy
       render json: { data: 'Error!' }, status: 500, content_type: 'text/json'
@@ -141,7 +141,7 @@ class SubmissionController < ApplicationController
     render json: { data: 'Error!' }, status: 500, content_type: 'text/json'
 
     @submission.log("Error creating submission: #{e.class} #{e.message}", "Error") if (@submission.persisted?)
-    STDERR.puts("Error creating submission: #{e.class} #{e.message}")
+    logger.error "Error creating submission: #{e.class} #{e.message}"
   end
 
   def edit_mark
