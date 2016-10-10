aefdef005fbd6ddd9aa291dadaa35b72bed8fcd0
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2016-10-10 14:51:36 +0100

    Create target folders before unzipping an entry
    Some zip tools do not include separate entries for directories. This was causing issues with unzipping as these directories were not created in the target folder.
    
    We're now creating directories always before unzipping an entry.

--- app/lib/submission_utils.rb
+++ app/lib/submission_utils.rb
@@ -21,8 +21,12 @@ class SubmissionUtils
           if file_blacklist.match(entry.name)
             submission.log("Ignored #{entry.name} because of blacklisting", 'Debug')
           else
+            entry_name = output_path.join(entry.name)
+            # Ensure the target directory exists even for zip files which do not contain them as separate items
+            FileUtils.mkdir_p File.dirname(entry_name)
+
+            entry.extract(entry_name)
             submission.log("Extracted #{entry.name}", 'Debug')
-            entry.extract(output_path.join(entry.name))
           end
         end
       end
