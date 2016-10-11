0ddb84d1ef73f1d7ebf6049409be6f8cf780909e
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2016-10-11 15:41:41 +0100

    Fixed error reporting

--- app/helpers/submission_helper.rb
+++ app/helpers/submission_helper.rb
@@ -1,7 +1,16 @@
 module SubmissionHelper
   def display_final_mark(submission)
     if submission.pending?
-      content_tag(:span, 'Pending', class: 'label label-default')
+      # This may actually be an error, so need to work our way through the various error states
+      if submission.extraction_error?
+        content_tag(:span, 'Extraction Error', class: 'label label-danger')
+      elsif (!submission.git_success?)
+        content_tag(:span, 'GHE Push Error', class: 'label label-danger')
+      elsif submission.failed?
+        content_tag(:span, 'Marking Tool Error', class: 'label label-danger')
+      else
+        content_tag(:span, 'Pending', class: 'label label-default')
+      end
     else
       s = "#{submission.mark}%"
       content_tag(:span) do
@@ -11,6 +20,7 @@ module SubmissionHelper
         else concat(content_tag(:strong, s, class: 'text-success'))
         end
         concat(content_tag(:span, ' '))
+        # TODO This next line is really only here to be backward compatible with when we were incorrectly reporting a mark of 0 against extraction errors
         concat(content_tag(:span, 'Extraction Error', class: 'label label-danger')) if submission.extraction_error?
         concat(content_tag(:span, 'Capped', class: 'label label-default')) if submission.late? && submission.assignment.late_capping?
       end
