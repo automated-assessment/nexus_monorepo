ae82922a39dacb14841cfab66287b4d34f8ae4d3
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-10-15 16:05:20 +0100

    Updated factory generator to include late deadline field

--- spec/factories/assignments.rb
+++ spec/factories/assignments.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     description { Faker::Hacker.say_something_smart }
     start { Faker::Time.between(DateTime.now.utc - 15.days, DateTime.now.utc + 36.days).beginning_of_hour }
     deadline { Faker::Time.between(start + 2.days, start + 62.days).beginning_of_hour }
+    latedeadline { Faker::Time.between(deadline + 1.days, deadline + 7.days).beginning_of_hour }
     allow_late { rand > 0.2 }
     late_cap { 40 }
     course
