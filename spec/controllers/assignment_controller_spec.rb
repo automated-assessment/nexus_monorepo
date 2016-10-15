3e1e8e9f04cfe5ea5331b0d661833cbadc19a137
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-10-15 13:37:12 +0100

    Set up test skeleton

--- spec/controllers/assignment_controller_spec.rb
+++ spec/controllers/assignment_controller_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe AssignmentController, type: :controller do
-  pending
+  describe 'GET #new' do
+
+  end
+
+  describe 'POST #create' do
+
+  end
+
+  describe 'GET #edit' do
+
+  end
+
+  describe 'POST #update' do
+
+  end
+
+  describe 'GET #show' do
+    it 'should show the assignment with the given id' do
+      
+    end
+
+    it 'should not show the assignment with an invalid id' do
+
+    end
+  end
+
+  describe 'GET #show_deadline_extensions' do
+
+  end
+
+  describe 'GET #quick_config_confirm' do
+
+  end
+
+  describe 'GET #configure_tools' do
+
+  end
+
+  describe 'GET #export_submissions_data' do
+
+  end
+
+  describe 'GET #list_submissions' do
+
+  end
 end
