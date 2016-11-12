0c7647e2144acd4a2d5767c481423e4c74492734
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-11-12 13:56:21 +0100

    Removed changes to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,8 +15,7 @@ if [ "$1" = 'init-js' ]; then
 	cd lib/web-ide
 	npm install --production --silent
 	cd ../..
-	npm install webpack
-	npm rebuild node-sass
+
 	exec npm run build
 fi
 
@@ -38,8 +37,7 @@ if [ "$1" = 'init' ]; then
         cd lib/web-ide
         npm install --production --silent
         cd ../..
-				npm install webpack
-				npm rebuild node-sass
+
         npm run build
 
         mkdir -p tmp/pids
