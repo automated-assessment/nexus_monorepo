c6d522345867b2cff71b2f89b5a98c5d8218a317
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-11-16 20:18:16 +0000

    Changed back to or condition to allow all admin users to configure courses. Updated tests to reflect this change

--- spec/helpers/course_helper_spec.rb
+++ spec/helpers/course_helper_spec.rb
@@ -13,9 +13,6 @@ RSpec.describe CourseHelper, type: :helper do
     it 'returns true for the teacher' do
       expect(user_can_administrate(c, t)).to be true
     end
-    it 'returns false for other teachers' do
-      expect(user_can_administrate(c, t2)).to be false
-    end
     it 'returns false for students' do
       expect(user_can_administrate(c, s)).to be false
     end
