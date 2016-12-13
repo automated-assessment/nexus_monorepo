afeaeda86e4cd899f0b6c78748ea6dd9ad8945f9
John armstrong <johnarmstrong5@googlemail.com>
2016-12-13 11:50:17 +0000

    Increase timeouts

--- python/fm06assessor.py
+++ python/fm06assessor.py
@@ -2,8 +2,8 @@ import matlabassessor
 from matlabrunner import MatlabFunction
 
 
-toRun = [ MatlabFunction("testall"),
-          MatlabFunction("question1", ["ret"]),
-          MatlabFunction("question2", ["part1","part2"])]
+toRun = [ MatlabFunction("testall", timeout=60),
+          MatlabFunction("question1", ["ret"], timeout=60),
+          MatlabFunction("question2", ["part1","part2"], timeout=60)]
 
 output = matlabassessor.run(toRun, dir='studentexample')
