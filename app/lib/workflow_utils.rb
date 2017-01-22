f1aae6252266ce2f10941aba291001ba48489aa5
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2017-01-22 20:09:59 +0000

    Moved workflow Construction algorithm to own libary

---
+++ app/lib/workflow_utils.rb
@@ -0,0 +1,35 @@
+class WorkflowUtils
+  class << self
+    # Given an `assignment` with some `Marking Tool Contexts`
+    # It creates a workflow using `Active Services`
+    # Returns an error message describing any invalid paramters
+    # Returns nil otherwise.
+    def construct_workflow(assignment)
+      tool_ids = assignment.marking_tool_contexts.pluck(:marking_tool_id)
+      assignment_tools = MarkingTool.where(id: tool_ids).pluck(:uid)
+
+      assignment.marking_tool_contexts.each do |mtc|
+        as = ActiveService.new
+        as.assignment = assignment
+        mt = MarkingTool.find(mtc.marking_tool_id)
+        as.marking_tool_uid = mt.uid
+        as.condition = mtc.condition
+
+        unless mtc.depends_on.empty?
+          # Check that active service depends on itself
+          if mtc.depends_on.include? mt.uid
+            return "#{mt.name} cannot depend on itself!"
+          end
+
+          # Check each item in depends on is actually a part of the assignment
+          unless (assignment_tools & mtc.depends_on).eql? mtc.depends_on
+            return "One or more tools that #{mt.name} depends on have not been added to the assignment"
+          end
+
+          as.parents = ActiveService.where(assignment_id: assignment.id, marking_tool_uid: mtc.depends_on)
+        end
+        as.save!
+      end
+    end
+  end
+end
