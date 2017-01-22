56f83aacbc3613a9cf2abeff981c41d317a6a012
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2017-01-22 20:09:44 +0000

    Removed need for depends on array to be constructed in assignment create action. Array is passed straight from view as part of the params

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,6 +1,7 @@
 class AssignmentController < ApplicationController
   include ApplicationHelper
   require_relative '../lib/git_utils'
+  require_relative '../lib/workflow_utils'
 
   before_action :authenticate_user!
 
@@ -68,44 +69,19 @@ class AssignmentController < ApplicationController
     return unless authenticate_admin!
     @assignment = Assignment.new(assignment_params)
     unless @assignment.save
-      flash[:error] = @assignment.errors.full_messages[0]
-      redirect_to action: 'new', cid: @assignment.course.id
-      @assignment.destroy
+      error_flash_and_cleanup!(@assignment.errors.full_messages[0])
       return
     end
 
     unless GitUtils.setup_remote_assignment_repo!(@assignment)
-      flash[:error] = 'Error creating repository for assignment!'
-      redirect_to action: 'new', cid: @assignment.course.id
-      @assignment.destroy
+      error_flash_and_cleanup!('Error creating repository for assignment!')
       return
     end
 
-    tool_ids = @assignment.marking_tool_contexts.pluck(:marking_tool_id)
-    assignment_tools = MarkingTool.where(id: tool_ids).pluck(:uid)
-    contexts = params[:assignment][:marking_tool_contexts_attributes].to_a
-    i = 0
-    @assignment.marking_tool_contexts.each do |mtc|
-      mtc.depends_on = contexts[i][1][:depends_on]
-      mtc.save
-      i += 1
-      as = ActiveService.new
-      as.assignment = @assignment
-      mt = MarkingTool.find(mtc.marking_tool_id)
-      as.marking_tool_uid = mt.uid
-      as.condition = mtc.condition
-
-      unless mtc.depends_on.empty?
-        unless (assignment_tools & mtc.depends_on).eql? mtc.depends_on
-          flash[:error] = "One or more tools that #{mt.name} depends on have not been added to the assignment"
-          redirect_to action: 'new', cid: @assignment.course.id
-          @assignment.destroy
-          return 1
-        end
-
-        as.parents = ActiveService.where(assignment_id: @assignment.id, marking_tool_uid: mtc.depends_on)
-      end
-      as.save!
+    message = WorkflowUtils.construct_workflow(@assignment)
+    if message
+      error_flash_and_cleanup!(message) if message
+      return
     end
 
     if @assignment.marking_tools.configurable.any?
@@ -204,7 +180,7 @@ class AssignmentController < ApplicationController
                                        :allow_zip,
                                        :allow_git,
                                        :allow_ide,
-                                       marking_tool_contexts_attributes: [:weight, :context, :marking_tool_id, :condition, :depends_on])
+                                       marking_tool_contexts_attributes: [:weight, :context, :marking_tool_id, :condition, depends_on: []])
   end
 
   def return_assignment!
@@ -214,5 +190,11 @@ class AssignmentController < ApplicationController
       render 'mine', status: 404
     end
     assignment
+  end
+
+  def error_flash_and_cleanup!(message)
+    flash[:error] = message
+    redirect_to action: 'new', cid: @assignment.course.id
+    @assignment.destroy
   end
 end
