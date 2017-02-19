d10bd01e2efc526b246f9bcc069b7f02492590cb
ellis246 <adam.ellis@kcl.ac.uk>
2017-02-19 23:32:47 +0000

    Rebuilt the allocation algorithm to allocate to 3 providers in db from this submission. Back tracks up to providerCount^2 to ensure no starvation. Could probably be further optimised.

--- server/controllers/allocation-process.js
+++ server/controllers/allocation-process.js
@@ -8,66 +8,78 @@
 //is this possible? - perhaps if one student submits many times
 //how to deal? - inform student that they can only submit so many times to fix this?
 
-const Form = require('../datasets/formModel.js');
+const Form = require('../datasets/formModel');
 const Submission = require('../datasets/submissionModel');
 
 
-module.exports.runAllocation = function(response){
-    Form.findOne({aid:response.aid})
-        .then(function(formData){
-            const providerTotal = formData.providerCount;
-            const minAllocationCount = formData.providerCount;
-            Submission.find({aid:response.aid})
-                .then(function(submissionData){
-                    if(submissionData.length>minAllocationCount){
-                        assign(providerTotal,submissionData,formData);
+module.exports.runAllocation = function(submission){
+    getAssignment(submission)
+        .then(function(assignment){
+            submissionCount(submission.aid)
+                .then(function(count){
+                    if(count<Math.pow(assignment.providerCount,2)){
+                        backTrack(assignment);
                     }
-                })
-        });
-
-
+                    allocate(assignment,submission);
+                });
+        })
 };
 
 
-const assign = function(providerTotal, submissionData,formData){
-    assignment(0);
-    function assignment(i){
-        if(i<providerTotal) {
-            const provider = randomStudent(submissionData);
-            const receiver = randomStudent(submissionData);
-            if((receiver.studentuid !== provider.studentuid) &&
-                receiver.providers.length <= minPid(submissionData)){
+const submissionCount = function(aid){
+    return Submission.count({aid:aid})
+        .then(function(response){
+            return response;
+        })
 
+};
+
+const backTrack = function(assignment){
+    Submission.find({$where:'this.providers.length < 3'})
+        .then(function(unallocated) {
+            unallocated.forEach(function(submission){
+                allocate(assignment,submission);
+            })
+        })
+};
+
+const getAssignment = function(submission){
+    return Form.findOne({aid:submission.aid},{_id:0,providerCount:1,formBuild:1})
+        .then(function(formData){
+            return formData;
+        })
+
+};
+
+const allocate = function(assignment,submission){
+    getProviders(assignment,submission)
+        .then(function(providers){
+            let providersArray = [];
+            providers.forEach(function(provider){
                 const providerObj = {
                     provideruid:provider.studentuid,
-                    currentForm:formData.formBuild,
+                    currentForm:assignment.formBuild,
                     provided:false
                 };
+                providersArray.push(providerObj);
+            });
 
-                receiver.providers.push(providerObj);
-                receiver.save()
-                    .then(function(response,err){
-                        assignment(++i);
-                    });
-            } else {
-                assignment(i);
-            }
-        }
-    }
+            submission.providers = providersArray;
+            submission.save();
+        })
 };
 
-const randomStudent = function(submissionData){
-    const submissionsLength = submissionData.length;
-    const studentIndex = Math.round(Math.random()*(submissionsLength-1));
-    return submissionData[studentIndex];
-
-};
-
-
-const minPid = function(submissionData){
-    const providersCount =[];
-    for(let submission in submissionData){
-        providersCount.push(submissionData[submission].providers.length);
-    }
-    return Math.min(...providersCount);
+const getProviders = function(assignment,submission){
+    return Submission.aggregate(
+        [
+            {$match:
+                {$and:
+                    [
+                        {studentuid:{$ne:submission.studentuid}},
+                        {aid:submission.aid}
+                    ]
+                }
+            },
+            {$sample:{size:assignment.providerCount}}
+        ])
 };
--- server/controllers/submission-controller.js
+++ server/controllers/submission-controller.js
@@ -4,18 +4,16 @@
 
 const Submission = require('../datasets/submissionModel');
 const responseController = require('./response-controller');
+const allocationProcess = require('./allocation-process');
 
 module.exports.createSubmission = function(req,res){
     res.status(200).send();
-    console.log(req.body);
     responseController.response(req);
 
     const submission = new Submission(req.body);
-    submission.save()
-        .then(function(response){
-            const allocationProcess = require('./allocation-process');
-            allocationProcess.runAllocation(response);
-        })
+
+    allocationProcess.runAllocation(submission);
+
 
 };
 
