a1686e42b0c2aa0530f7d288799a19a37fd0e35d
k1623121 <k1623121@kcl.ac.uk>
2017-06-06 16:57:52 +0100

    Basic listener code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# unique-assignment-tool
+## Nexus - Unique assingment Tool
+
+### Tech
+- Node.js (4.x)
+  - Express (4.x)
+  - _See `package.json` for full list of packages_
+
+### More details will be added...
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
+
+const app = express();
+const port = 3009;
+const accessToken = process.env.NEXUS_ACCESS_TOKEN || "foo";
+
+app.use(bodyParser.json());
+app.use(errorhandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+
+app.post('/unique_gen', function (request, response) {
+    console.log('Unique assignment file generation request received.')
+    if(request.headers["nexus-access-token"] == accessToken) {
+        console.log(`Request for fetching files to mark assignment ${request.body.aid}, 
+        submission id: ${request.body.sid},
+        for student with id: ${request.body.studentuid}`);
+		
+		//Collecting template parameters and their desired types
+			
+        //TODO Aydin: Do generation.
+        //TODO Aydin: Put generated files
+		
+        //TODO Aydin: Put acanycess token here as well for auth.
+        console.log('Sent Hello World, ish...');
+        response.send('Unique generation is still under construction.');
+    }
+    else {
+      console.log('Token error. The token:');
+      console.log(request.headers["nexus-access-token"])
+      response.send('Invalid token!');
+    }
+})
+
+app.listen(port, function () {
+    console.log('Test UAT listening on port: ' + port);
+})
