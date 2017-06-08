4bf67018c15f40ccdd582b7e280fbe187bf04d45
k1623121 <k1623121@kcl.ac.uk>
2017-06-08 18:14:05 +0100

    Description handling code

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var errorhandler = require('errorhandler');
+var request = require('request');
 
 const app = express();
 const port = 3009;
-const accessToken = process.env.NEXUS_ACCESS_TOKEN || "foo";
+const accessToken = process.env.NEXUS_ACCESS_TOKEN;
+const NEXUS_BASE_URL = process.env.NEXUS_BASE_URL || 'http://localhost:3000';
+
+if (!process.env.NEXUS_ACCESS_TOKEN) {
+  console.log('Error: Specify NEXUS_ACCESS_TOKEN in environment');
+  process.exit(1);
+}
 
 app.use(bodyParser.json());
 app.use(errorhandler({
@@ -12,9 +19,21 @@ app.use(errorhandler({
     showStack: true
 }));
 
+app.post('/desc_gen', function (request, response) {
+    console.log('Unique assignment desscription generation request received.');
+	console.log(`Request for generating description for assignment with id: ${request.body.aid},
+	for student with id: ${request.body.studentid}`);
+
+	//TODO Aydin: Do generation.
+
+	console.log(`Sent description for assignment with id:{request.body.aid},
+	for student with id: ${request.body.studentid}`);
+	response.send('Unique generation is still under construction.');
+})
+
 app.post('/unique_gen', function (request, response) {
     console.log('Unique assignment file generation request received.')
-    if(request.headers["nexus-access-token"] == accessToken) {
+    if(request.headers["nexus-access-token"] == accessToken) { 
         console.log(`Request for fetching files to mark assignment ${request.body.aid}, 
         submission id: ${request.body.sid},
         for student with id: ${request.body.studentuid}`);
