922656b3af844d4d3da676cc99deeb09ec63365b
k1623121 <k1623121@kcl.ac.uk>
2017-06-09 14:15:56 +0100

    Initial changes on the code and adding necessary files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var errorhandler = require('errorhandler');
-var request = require('request');
+var fs = require('fs');
 
 const app = express();
 const port = 3009;
@@ -13,6 +13,12 @@ if (!process.env.NEXUS_ACCESS_TOKEN) {
   process.exit(1);
 }
 
+//TODO Aydin: Solely for initial generation functionality. Values will be fetched from db in later stages
+var valueArray = new Array();
+valueArray.push(new Array());
+valueArray.push(new Array());
+valueArray.push(new Array());
+
 app.use(bodyParser.json());
 app.use(errorhandler({
     dumpExceptions: true,
@@ -23,6 +29,38 @@ app.post('/desc_gen', function (request, response) {
     console.log('Unique assignment desscription generation request received.');
 	console.log(`Request for generating description for assignment with id: ${request.body.aid},
 	for student with id: ${request.body.studentid}`);
+	
+	var studentID = request.body.studentid;
+	var descriptionString = request.body.description_string;
+	
+	//Collecting template parameters and their desired types
+	var parameterStr = request.body.parameter_string;
+	var parameterPairs = parameterStr.split([',']);
+	var parameterNameArr = new Array();
+	var parameterTypeArr = new Array();
+
+	for(var i = 0; i < parameterPairs.length; i++) {
+		parameterNameArr[i] = parameterPairs[i].split(':')[0];
+		parameterTypeArr[i] = parameterPairs[i].split(':')[1];
+	}
+	
+	var lineReader1 = require('readline').createInterface({
+	  input: require('fs').createReadStream(process.cwd()+'/1')
+	});
+
+	lineReader1.on('line', function (line) {
+	  valueArray[1].push(line);
+	});
+
+	var lineReader2 = require('readline').createInterface({
+	  input: require('fs').createReadStream(process.cwd()+'/1')
+	});
+
+	lineReader2.on('line', function (line) {
+	  valueArray[2].push(line);
+	});
+	
+	console.log(valueArray);
 
 	//TODO Aydin: Do generation.
 
@@ -56,7 +94,7 @@ app.post('/unique_gen', function (request, response) {
         //TODO Aydin: Do generation.
         //TODO Aydin: Put generated files
 		
-        //TODO Aydin: Put acanycess token here as well for auth.
+        //TODO Aydin: Put access token here as well for auth.
         console.log('Sent Hello World, ish...');
         response.send('Unique generation is still under construction.');
     }
