8ae618ac51a2ef67d07f9b9c5f12382c8677ef86
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2017-07-08 09:58:55 +0100

    replaced unless with if/else. Updated flash to include all messages in the flash.now hash

--- app/controllers/marking_tool_controller.rb
+++ app/controllers/marking_tool_controller.rb
@@ -19,14 +19,14 @@ class MarkingToolController < ApplicationController
 
   def update
     marking_tool = return_marking_tool!
-    unless marking_tool.update_attributes(marking_tool_params)
-      flash[:error] = marking_tool.errors.full_messages[0]
+    if marking_tool.update_attributes(marking_tool_params)
+      flash.now[:success] = 'Marking Tool updated'
+      redirect_to admin_panel_path
+    else
+      flash.now[:error] = marking_tool.errors.full_messages
       render 'edit'
       return
     end
-
-    flash[:success] = 'Marking Tool updated'
-    redirect_to admin_panel_path
   end
 
   def destroy
