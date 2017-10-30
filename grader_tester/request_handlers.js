ad9c4ef0320bcb9c12c0d5717c6df7b678ec96df
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2017-10-30 19:37:12 +0000

    Set up basic test script

--- grader_tester/request_handlers.js
+++ grader_tester/request_handlers.js
@@ -1,6 +1,7 @@
 import async from 'async';
 import forEachOf from 'async/eachOf';
 import series from 'async/series';
+import yaml from 'node-yaml';
 
 const fs = require('fs-extra');
 
@@ -21,6 +22,14 @@ export function handle_receive_feedback (request, response) {
 export function start_tests() {
   console.log ("Starting to run test.");
 
-  console.log ("All tests have run.");
-  process.exit(0);
+  yaml.read ("/test-specs/tests.yml", {schema: yaml.schema.defaultSafe}, (err, data) => {
+    if (err) {
+      console.log(`Error reading test specification: ${err}.`);
+    } else {
+      console.log("Read test specification, getting ready to run tests...");
+    }
+
+    console.log ("All tests have run.");
+    process.exit(0);
+  });
 }
