cb96077f1bb7f1f933ea7eb25524bc91efb7a46f
Kyle Hodgetts (khodgetts) <t-khodgetts@expedia.com>
2017-11-29 10:19:54 +0000

    Script now checks for status code when deleting repo and echos number of repos deleted at the end.

--- cleanup-repos.sh
+++ cleanup-repos.sh
@@ -11,18 +11,38 @@ if [[ -z $org ]] || [[ -z $token ]]; then
   exit 1
 fi
 
+echo "Are you sure you want to delete all assignment repositories in the $org organisation. [Y|y]"
+read prompt
+
+if [[ $prompt -ne "Y" ]] || [[ $prompt -ne "y" ]]; then
+  exit 0
+fi
+
 # Required header for api call
 token_cmd="Authorization: token $token"
 
 # Get all repos and filter so we are left with an array of repo names
 # in the form organisation/repo_name
-repos=( $(curl -H "$token_cmd" "$url/orgs/$org/repos" | jq -r '.[].full_name') )
+repos=( $(curl -H "$token_cmd" --silent "$url/orgs/$org/repos" | jq -r '.[].full_name') )
 
+repos_deleted=0
 # Delete each repo, echoing out each deleted repo name
 for repo in ${repos[@]}
 do
-  echo $repo
-  curl -X "DELETE" -H "$token_cmd" "$url/repos/$repo"
-done
+  # Check that repo to delete starts with the organisation/assignment-
+  if [[ "$repo" =~ ^$org/assignment-* ]]; then
+    echo "Deleting $repo"
+    # Delete if assignment name is prefixed with `org/assignment-`
+    response=$(curl -X "DELETE" -H "$token_cmd" --silent --write-out '%{http_code}' "$url/repos/$repo")
 
+    # If curl yields a 403
+    # exit loop because org has forbidden repo deletion by members
+    if [[ $response == 403 ]]; then
+      echo "You do not have permission to remove organisation repositories"
+      break
+    fi
+    repos_deleted=$((repos_deleted+=1))
+  fi
+done
+echo "$repos_deleted repositories deleted from $org"
 exit 0
