ca480a130dc545c45032b0ac7bf4d7fd83afc18a
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2018-04-26 12:07:38 +0100

    Ensure only submitted java files are considered

--- junit-grader/grade_submission.sh
+++ junit-grader/grade_submission.sh
@@ -4,18 +4,18 @@ echo "Compiling test code and submission"
 
 BIN_DIR=$(mktemp -d)
 
-SUBMISSION_CLASSPATH=".:$(find . -name '*.jar' | paste -s -d':')"
-TEST_CLASSPATH="/usr/src/app/bin/:/usr/src/app/junit-4.12.jar:/usr/src/app/hamcrest-core-1.3.jar:$test_files/:$(find $test_files/ -name '*.jar' | paste -s -d':'):$BIN_DIR:$(find . -name '*.jar' | paste -s -d':')"
+SUBMISSION_CLASSPATH="$(find . -name '*.jar' | paste -s -d':')"
+TEST_CLASSPATH="/usr/src/app/bin/:/usr/src/app/junit-4.12.jar:/usr/src/app/hamcrest-core-1.3.jar:$(find $test_files/ -name '*.jar' | paste -s -d':'):$BIN_DIR:$(find . -name '*.jar' | paste -s -d':')"
 
 echo "Compiling submission"
-if ! javac -cp $SUBMISSION_CLASSPATH -d $BIN_DIR $(find . -name '*.java'); then
+if ! javac -cp "$SUBMISSION_CLASSPATH" -d $BIN_DIR $(find . -name '*.java'); then
   rm -rf $BIN_DIR
   echo "<p>Failed to compile your submission code.</p>" > $1
   exit 0
 fi
 
 echo "Compiling test cases against submission"
-if ! javac -cp $TEST_CLASSPATH -d $BIN_DIR $(find $test_files/ -name '*.java'); then
+if ! javac -cp "$TEST_CLASSPATH" -d $BIN_DIR $(find $test_files/ -name '*.java'); then
   rm -rf $BIN_DIR
   echo "<p>Failed to compile tests against your code. Have you followed all naming conventions and method signatures defined by the assignment?</p>" > $1
   exit 0
@@ -30,7 +30,7 @@ if [ $? -ne 0 ]; then
   exit -1
 fi
 
-java -cp $TEST_CLASSPATH uk.ac.kcl.inf.nexus.junit_grader.TestRunner $1 $MARK_FILE
+java -cp "$TEST_CLASSPATH" uk.ac.kcl.inf.nexus.junit_grader.TestRunner $1 $MARK_FILE
 if [ $? -ne 0 ]; then
   rm -rf $BIN_DIR
   rm -f $MARK_FILE
